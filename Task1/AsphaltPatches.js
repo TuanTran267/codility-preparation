a2b75fea1f20ab7d98c677bdec58566ec2753c09
TuanTran267 <tranhoangtuan2607@gmail.com>
2024-06-10 10:03:02 +0700

    add comments

--- Task1/AsphaltPatches.js
+++ Task1/AsphaltPatches.js
@@ -1,14 +1,20 @@
 const asphaltPatches = (arr) => {
     
+    // If the array is empty or there is no 'X' in the array, return 0
     if (arr.length === 0 || !arr.split('').some((item) => item === 'X')) return 0;
+    
+    // If all the elements in the array are 'X', return the ceiling of the length of the array divided by 3
     if (arr.split('').every((item) => item === 'X')) return Math.ceil(arr.length/3);
 
     let count = 0;
     let i = 0;
+    // Loop through the array
     while (i < arr.length) {
+        // If the current element is 'X', increment i by 3 and increment count by 1
         if (arr[i] === 'X') {
             i = i + 3;
             count++;
+            // If the current element is not 'X', increment i by 1
         } else {
             i++;
         }
